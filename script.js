b2fcfd86e0d5242f3daa0f7620c9805d5ef896b6
edwin2011-la <edwinakobeh789@gmail.com>
2024-02-28 14:22:01 -0800

    commit

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let hideButton = document.getElementById("hide");
 let dialog = document.getElementById("dialog");
 let addButton = document.getElementById("add");
 
+let input = document.getElementById("title");
+let textArea = document.getElementById("textarea");
+let notesArr = [];
+
+let notes = document.querySelector(".container");
+
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
@@ -11,6 +17,25 @@ hideButton.addEventListener("click", () => {
   dialog.close();
 });
 
+// input.addEventListener("change", () => {
+//   console.log("car");
+// });
+
 addButton.addEventListener("click", () => {
-  
+  let newTask = {
+    id: crypto.randomUUID(),
+    title: input.value,
+    text: textArea.value,
+  };
+
+  notesArr.push(newTask);
+
+notesArr.map(element => {
+  let list = document.createElement("p")
+  list.textContent = element.title
+  notes.append(list)
+})
+  console.log(notesArr);
+
 });
+
